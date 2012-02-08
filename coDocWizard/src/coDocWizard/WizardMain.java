c4e6d7549abbc04e3da1cc9edb22501ad774a9cb
Derek QIAN <electronseu@gmail.com>
2012-02-07 16:07:48 -0800

    coDocWizard template

--- coDocWizard/src/coDocWizard/WizardMain.java
+++ coDocWizard/src/coDocWizard/WizardMain.java
@@ -4,33 +4,40 @@ package coDocWizard;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.core.runtime.*;
-import org.eclipse.jface.operation.*;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.CoreException;
 import java.io.*;
-import org.eclipse.ui.*;
+
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
 import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class WizardMain extends Wizard implements INewWizard 
+public class WizardMain extends Wizard implements INewWizard, IExecutableExtension
 {
     private WelcomePage page1;
-    private ProjectCreationPage page2;
+    private WizardNewProjectCreationPage page2;
+	private IConfigurationElement config;
     private IWorkbench workbench;
-	private ISelection selection;
+	private IStructuredSelection selection;
+	private IProject project;
     
     public WizardMain() 
     {
-         super();
-         setNeedsProgressMonitor(true);
-     }
+    	super();
+    }
 
 	public void init(IWorkbench workbench, IStructuredSelection selection) 
     {
@@ -38,6 +45,11 @@ public class WizardMain extends Wizard implements INewWizard
         this.selection = selection;
         setWindowTitle("Generate a new coDoc project");
     }
+	
+	public void setInitializationData(IConfigurationElement config, String propertyName, Object data) throws CoreException 
+	{
+		this.config = config;
+	}
 
     /** 
      * @see org.eclipse.jface.wizard.Wizard#addPages()
@@ -47,7 +59,9 @@ public class WizardMain extends Wizard implements INewWizard
         super.addPages();
         page1 = new WelcomePage();
         addPage(page1);
-        page2 = new ProjectCreationPage(selection);
+        page2 = new WizardNewProjectCreationPage("page2");
+        page2.setTitle("Create a new coDoc project");
+        page2.setDescription("Create a new coDoc project.");
         addPage(page2);
     }
 
@@ -56,30 +70,39 @@ public class WizardMain extends Wizard implements INewWizard
      */
     public boolean performFinish() 
     {
-		final String containerName = page2.getContainerName();
-		final String fileName = page2.getFileName();
-		IRunnableWithProgress op = new IRunnableWithProgress() 
+		if (project != null) 
 		{
-			public void run(IProgressMonitor monitor) throws InvocationTargetException 
+			return true;
+		}
+
+		final IProject projectHandle = page2.getProjectHandle();
+
+		URI projectURI = (!page2.useDefaults()) ? page2.getLocationURI() : null;
+
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+
+		final IProjectDescription desc = workspace.newProjectDescription(projectHandle.getName());
+
+		desc.setLocationURI(projectURI);
+
+		/*
+		 * Just like the NewFileWizard, but this time with an operation object
+		 * that modifies workspaces.
+		 */
+		WorkspaceModifyOperation op = new WorkspaceModifyOperation() {
+			protected void execute(IProgressMonitor monitor) throws CoreException 
 			{
-				try 
-				{
-					doFinish(containerName, fileName, monitor);
-				} 
-				catch (CoreException e) 
-				{
-					throw new InvocationTargetException(e);
-				} 
-				finally 
-				{
-					monitor.done();
-				}
+				createProject(desc, projectHandle, monitor);
 			}
 		};
+
+		/*
+		 * This isn't as robust as the code in the BasicNewProjectResourceWizard
+		 * class. Consider beefing this up to improve error handling.
+		 */
 		try 
 		{
-			//run(boolean fork, boolean cancelable, IRunnableWithProgress runnable)
-			getContainer().run(true, false, op);
+			getContainer().run(true, true, op);
 		} 
 		catch (InterruptedException e) 
 		{
@@ -91,66 +114,118 @@ public class WizardMain extends Wizard implements INewWizard
 			MessageDialog.openError(getShell(), "Error", realException.getMessage());
 			return false;
 		}
+
+		project = projectHandle;
+
+		if (project == null) 
+		{
+			return false;
+		}
+
+		BasicNewProjectResourceWizard.updatePerspective(config);
+		BasicNewProjectResourceWizard.selectAndReveal(project, workbench.getActiveWorkbenchWindow());
+
 		return true;
     }
-
-	private void doFinish(String containerName, String fileName, IProgressMonitor monitor) throws CoreException 
+    
+	void createProject(IProjectDescription description, IProject proj, IProgressMonitor monitor) throws CoreException, OperationCanceledException 
 	{
-		// create a sample file
-		monitor.beginTask("Creating " + fileName, 2);
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IResource resource = root.findMember(new Path(containerName));
-		if (!resource.exists() || !(resource instanceof IContainer)) 
-		{
-			throwCoreException("Container \"" + containerName + "\" does not exist.");
-		}
-		IContainer container = (IContainer) resource;
-		final IFile file = container.getFile(new Path(fileName));
 		try 
 		{
-			InputStream stream = openContentStream();
-			if (file.exists()) 
+			monitor.beginTask("", 2000);
+
+			proj.create(description, new SubProgressMonitor(monitor, 1000));
+
+			if (monitor.isCanceled()) 
 			{
-				file.setContents(stream, true, true, monitor);
+				throw new OperationCanceledException();
+			}
+
+			proj.open(IResource.BACKGROUND_REFRESH, new SubProgressMonitor(monitor, 1000));
+
+			/*
+			 * Okay, now we have the project and we can do more things with it
+			 * before updating the perspective.
+			 */
+			IContainer container = (IContainer) proj;
+
+			/* Add an readme file */
+			addFileToProject(container, new Path("readme.txt"), openContentStream(), monitor);
+			/* Add an cdc file */
+			InputStream resourceStream1 = this.getClass().getResourceAsStream("templates/cdc-template.resource");
+			addFileToProject(container, new Path("template.cdc"), resourceStream1, monitor);
+			resourceStream1.close();
+			
+			/* Add the style folder and the site.css file to it */
+			final IFolder specFolder = container.getFolder(new Path("spec"));
+			specFolder.create(true, true, monitor);
+			InputStream resourceStream2 = this.getClass().getResourceAsStream("templates/pdf-sample.resource");
+			final IFile file2 = container.getFile(new Path(specFolder.getName() + Path.SEPARATOR + "sample.pdf"));
+			if (file2.exists()) 
+			{
+				file2.setContents(resourceStream2, true, true, monitor);
 			} 
 			else 
 			{
-				file.create(stream, true, monitor);
+				file2.create(resourceStream2, true, monitor);
 			}
-			stream.close();
-		} 
-		catch (IOException e) 
-		{
-		}
-		monitor.worked(1);
-		monitor.setTaskName("Opening file for editing...");
-		getShell().getDisplay().asyncExec(new Runnable() {
-			public void run() 
-			{
-				IWorkbenchPage page =
-					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-				try 
+			resourceStream2.close();
+			
+			/*
+			 * Add the code folder.
+			 */
+			IFolder codeFolder = container.getFolder(new Path("code"));
+			codeFolder.create(true, true, monitor);
+			InputStream resourceStream3 = this.getClass().getResourceAsStream("templates/c-sample.resource");
+			addFileToProject(container, new Path(codeFolder.getName() + Path.SEPARATOR + "sample.c"), resourceStream3, monitor);
+			resourceStream3.close();
+
+			monitor.worked(1);
+			monitor.setTaskName("Opening file for editing...");
+			getShell().getDisplay().asyncExec(new Runnable() {
+				public void run() 
 				{
-					IDE.openEditor(page, file, true);
-				} 
-				catch (PartInitException e) 
-				{
+					IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+					try 
+					{
+						IDE.openEditor(page, file2, true);
+					} 
+					catch (PartInitException e) 
+					{
+					}
 				}
-			}
-		});
-		monitor.worked(1);
+			});
+			monitor.worked(1);
+		} 
+		catch (IOException ioe) 
+		{
+			IStatus status = new Status(IStatus.ERROR, "coDocWizard", IStatus.OK, ioe.getLocalizedMessage(), null);
+			throw new CoreException(status);
+		} 
+		finally 
+		{
+			monitor.done();
+		}
+	}
+	
+	private void addFileToProject(IContainer container, Path path, InputStream contentStream, IProgressMonitor monitor) throws CoreException 
+	{
+		final IFile file = container.getFile(path);
+
+		if (file.exists()) 
+		{
+			file.setContents(contentStream, true, true, monitor);
+		} 
+		else 
+		{
+			file.create(contentStream, true, monitor);
+		}
 	}
 
 	private InputStream openContentStream() 
 	{
 		String contents =
-			"coDoc: This is the initial file contents for *.mpe file that should be word-sorted in the Preview page of the multi-page editor";
+			"coDoc: readmefile.";
 		return new ByteArrayInputStream(contents.getBytes());
-	}
-
-	private void throwCoreException(String message) throws CoreException 
-	{
-		IStatus status = new Status(IStatus.ERROR, "NewFileWizard", IStatus.OK, message, null);
-		throw new CoreException(status);
 	}
 }
