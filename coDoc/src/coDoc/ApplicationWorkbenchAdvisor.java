76cf2aaf9ac583caedc2bc4610a315b74c87dca6
Derek QIAN <electronseu@gmail.com>
2012-08-09 12:11:16 -0700

    display the icons in Project Explorer

--- coDoc/src/coDoc/ApplicationWorkbenchAdvisor.java
+++ coDoc/src/coDoc/ApplicationWorkbenchAdvisor.java
@@ -1,13 +1,20 @@
 package coDoc;
 
+import java.net.URL;
+
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.osgi.framework.Bundle;
+
 import edu.pdx.svl.coDoc.cdc.perspective.Perspective;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
@@ -20,8 +27,20 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
         super.initialize(configurer);
         configurer.setSaveAndRestore(true);
         IDE.registerAdapters();
+        
+        // display the icons in Project Explorer
+    	final String ICONS_PATH = "icons/full/";
+    	final String PATH_OBJECT = ICONS_PATH + "obj16/";
+    	Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
+    	declareWorkbenchImage(configurer, ideBundle, IDE.SharedImages.IMG_OBJ_PROJECT, PATH_OBJECT + "prj_obj.gif", true);
+    	declareWorkbenchImage(configurer, ideBundle, IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, PATH_OBJECT + "cprj_obj.gif", true);
     }
-
+    private void declareWorkbenchImage(IWorkbenchConfigurer configurer_p, Bundle ideBundle, String symbolicName, String path, boolean shared) {
+    	URL url = ideBundle.getEntry(path);
+    	ImageDescriptor desc = ImageDescriptor.createFromURL(url);
+    	configurer_p.declareImage(symbolicName, desc, shared);
+    }
+    
 	public String getInitialWindowPerspectiveId() {
 		//return Perspective.ID;
 		return Perspective.ID;
