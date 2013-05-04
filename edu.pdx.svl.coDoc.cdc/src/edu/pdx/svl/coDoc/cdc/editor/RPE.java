f38a2d8ea50a8abb5cc6f2d1c2af5558021210aa
derekqian <electronseu@gmail.com>
2013-05-04 01:36:14 -0700

    RPE evaluation

---
+++ edu.pdx.svl.coDoc.cdc/src/edu/pdx/svl/coDoc/cdc/editor/RPE.java
@@ -0,0 +1,10 @@
+package edu.pdx.svl.coDoc.cdc.editor;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+
+public class RPE {
+	public static void main(String[] args) {
+		System.out.println("RPE,");
+		return;
+	}
+}
